1632e9c44ef6fe4404050cc2395d6f08c0f16bff
unknown <kimchoxd@gmail.com>
2022-02-11 16:19:42 -0600

    commit

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,2 @@
 @echo off
-npm run build&&cd dist&&git init&&git add -A&&git commit -m 'deploy'&&git push -f https://github.com/kimch0/Project-Vue.git master:gh-pages&&cd ..
+npm install&&npm run serve&&npm run build&&cd dist&&git init&&git add -A&&git commit -m 'deploy'&&git push -f https://github.com/kimch0/Project-Vue.git master:gh-pages&&cd ..
